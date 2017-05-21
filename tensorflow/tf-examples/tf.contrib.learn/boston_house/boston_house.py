1b706067fdbc9cdda74d4523edbaac63585034fc
lienhua34 <lienhua34@163.com>
2017-05-22 00:11:35 +0800

    boston house 样例代码添加模型导出

--- tensorflow/tf-examples/tf.contrib.learn/boston_house/boston_house.py
+++ tensorflow/tf-examples/tf.contrib.learn/boston_house/boston_house.py
@@ -3,9 +3,13 @@ from __future__ import division
 from __future__ import print_function
 
 import itertools
+import tempfile
 
 import pandas as pd
 import tensorflow as tf
+
+from tensorflow.contrib.layers import create_feature_spec_for_parsing
+from tensorflow.contrib.learn.python.learn.utils import input_fn_utils
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -45,9 +49,20 @@ ev = regressor.evaluate(input_fn=lambda: input_fn(test_set), steps=1)
 loss_score = ev["loss"]
 print("Loss: {0:f}".format(loss_score))
 
-
 # Making Predictions
 y = regressor.predict(input_fn=lambda: input_fn(prediction_set))
 # .predict() returns an iterator; convert to a list and print predictions
 predictions = list(itertools.islice(y, 6))
 print ("Predictions: {}".format(str(predictions)))
+
+# export model
+feature_spec = create_feature_spec_for_parsing(feature_cols)
+serving_input_fn = input_fn_utils.build_parsing_serving_input_fn(feature_spec)
+
+export_model_dir = tempfile.mkdtemp()
+export_model_path = regressor.export_savedmodel(
+    export_model_dir,
+    serving_input_fn)
+print("exported model path: {}".format(export_model_path))
+
+
